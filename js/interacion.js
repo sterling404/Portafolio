3dd48d4cb28a345906cde58897a7f7fd81392b14
sterling404 <107717253+sterling404@users.noreply.github.com>
2023-06-06 11:29:51 -0400

    Add files via upload

---
+++ js/interacion.js
@@ -0,0 +1,48 @@
+/*if(document.getElementById("formm").value){
+    window.alert("los datos fue enviado con exito");
+}*/
+const btnSwitch = document.querySelector("#switch");
+const menu = document.querySelector("#icono");
+
+
+menu.addEventListener("click", () =>{
+    if(menu.classList.contains('bx-menu')){
+        menu.classList.remove('bx-menu');
+        menu.classList.toggle("bx-x");
+
+    }else {
+        menu.classList.remove('bx-x')
+        menu.classList.toggle("bx-menu");
+    }
+       
+})
+
+btnSwitch.addEventListener("click", () =>{
+    document.body.classList.toggle("claro");
+    btnSwitch.classList.toggle("active");
+
+    //vamos a guardar el modo en local storage
+    if(document.body.classList.contains("claro")){
+        localStorage.setItem('modo', 'true');
+    } else{
+        localStorage.setItem('modo', 'false');
+    }
+    
+})
+//obtenemos el modo actual
+
+if(localStorage.getItem('modo')==='true'){
+    document.body.classList.add("claro");
+    btnSwitch.classList.add("active");
+}else{
+    document.body.classList.remove('claro');
+    btnSwitch.classList.remove("active");
+}
+document.querySelector('form')
+if(addEventListener('submit')){
+    alert("gracias por llenar el formulario")
+}
+
+function avisos(){
+    window.alert('Gracias por completar este formulario')
+}
